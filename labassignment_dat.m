21c201f48b03a1e30cfc10cd5dc997056be06cdf
Rickard <richen_mjau@hotmail.com>
2016-09-15 15:09:30 +0200

    added dat file for constants

---
+++ labassignment_dat.m
@@ -0,0 +1,13 @@
+% This file contains all constants and will
+% load them into matlab workspace during 
+% preload of the simulink model.
+
+
+% Constants given in task
+J_1= 1.7*10^-3;
+J_2= 4.8*10^-3;
+N= 64;
+K_lin=1.19;
+R_m=3.21;
+K_m=8.39*10^-3;
+
